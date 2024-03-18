5ed4b57bb8ed4c2771bb2dceb090402b4cbd69e4
author name <test@test.test>
2024-03-18 19:17:50 +0100

    vfd

--- src/archivebackend/admin/Edition.py
+++ src/archivebackend/admin/Edition.py
@@ -3,4 +3,5 @@ from archivebackend.admin.AbstractAdmin import RemoteAdminView
 
 class EditionAdmin(RemoteAdminView):
     autocomplete_fields = ["additional_authors"]
+    readonly_fields = ["last_file_update", "file_url"]
     pass
