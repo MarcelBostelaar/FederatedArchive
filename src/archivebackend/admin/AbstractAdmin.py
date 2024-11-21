0d75197923b4bb6428781e163e3397190bc88f46
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2024-11-21 20:59:23 +0100

    Removed old attempts at admin models

--- src/archivebackend/admin/AbstractAdmin.py
+++ src/archivebackend/admin/AbstractAdmin.py
@@ -26,19 +26,4 @@ class RemoteAdminView(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return self.has_change_permission(request, obj)
     
-def make_aliases(modeladmin, request, queryset):
-    if queryset.count() < 2:
-        return
-    first = queryset[0]
-    for i in queryset:
-        first.addAlias(i)
-    first.fixAllAliases()
 
-class AliasableRemoteAdminView(RemoteAdminView):
-
-    def get_exclude(self, request, obj=None):
-        fromSuper = super().get_exclude(request, obj)
-        return ([] if fromSuper is None else list(fromSuper)) + ["alias_identifier"]
-    
-    inlines = [] #TODO add in selection menu
-    actions = [make_aliases]
--- src/archivebackend/admin/__init__.py
+++ src/archivebackend/admin/__init__.py
@@ -1,30 +1,33 @@
 from django.contrib import admin
-from archivebackend.admin.AbstractAdmin import AliasableRemoteAdminView, RemoteAdminView
-from archivebackend.admin.Authors import AuthorAdmin
-from archivebackend.admin.Document import DocumentAdmin
-from archivebackend.admin.Edition import EditionAdmin
-from archivebackend.admin.InlineTranslations import *
-from archivebackend.admin.RemoteViewPeer import RemotePeerView
+from archivebackend.admin.AbstractAdmin import RemoteAdminView
 from archivebackend.models import *
 
-admin.site.register(RemotePeer, RemotePeerView)
-admin.site.register(FileFormat, AliasableRemoteAdminView)
-admin.site.register(Language, AliasableRemoteAdminView)
-admin.site.register(Author, AuthorAdmin)
-admin.site.register(AuthorDescriptionTranslation, RemoteAdminView)
-admin.site.register(AbstractDocument, DocumentAdmin)
-admin.site.register(AbstractDocumentDescriptionTranslation, RemoteAdminView)
-admin.site.register(Edition, EditionAdmin)
-admin.site.register(Revision, RemoteAdminView)
-admin.site.register(File, RemoteAdminView)
-admin.site.register(AutoGenerationConfig)
-admin.site.register(AutoGeneration)
 
-class dummy(admin.ModelAdmin):
+class AliasAdminModel(admin.ModelAdmin):
     readonly_fields = ('alias_identifier',)
     list_display = ["origin", "target", "alias_identifier"]
     
     def alias_identifier(self, obj):
         return obj.origin.alias_identifier
 
-admin.site.register(archiveAppConfig.get_model("AuthorAliasThrough"), dummy)
+
+remotes = [
+    RemotePeer, AuthorDescriptionTranslation,
+    AbstractDocumentDescriptionTranslation,
+    Edition, Revision, File 
+           ]
+
+remotesAliasable = [
+    FileFormat, Language, Author, AbstractDocument
+]
+
+for i in remotes:
+    admin.site.register(i, RemoteAdminView)
+
+for i in remotesAliasable:
+    admin.site.register(i, RemoteAdminView)
+    admin.site.register(archiveAppConfig.get_model(i.__name__ + "AliasThrough"), AliasAdminModel)
+    
+
+admin.site.register(AutoGenerationConfig)
+admin.site.register(AutoGeneration)
