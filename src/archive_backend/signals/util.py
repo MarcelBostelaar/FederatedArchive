5a390a380e18a54f3e957972be3ef0f1b7c8a70a
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2024-12-04 21:51:12 +0100

    Split pre_save_filter into three disctinct filters
    makes it easier to reason about, since 2 of them never fire on new items but one does

--- src/archive_backend/signals/util.py
+++ src/archive_backend/signals/util.py
@@ -8,42 +8,99 @@ def not_new_items():
         return internal_filter
     return inner
 
-def pre_save_value_filter(newValuesMustContain={}, valuesMustHaveChanged = [], oldValuesMustBe={}):
-    """Utility function to stop firing a signal if the model instance does not meet the criteria.
-    hasValues is a dictionary of the values that must be set in the new data content of the model.
-    changeinAnyValues is a list of values that must be changed in the new data content of the model.
-    Prevents firing of signal on object creation"""
+def pre_save_new_values(**signalkwargs):
+    """Utility function to stop firing a pre_save signal if the model instance does not meet the criteria.
+
+    The values of the instance must meet the criteria in the kwargs dictionary"""
     def inner(func):
         def internal_filter(sender = None, instance = None, *args, **kwargs):
-            for (key, value) in newValuesMustContain.items():
+            for (key, value) in signalkwargs.items():
                 if not hasattr(instance, key):
                     raise Exception("Attribute " + key + " does not exist")
                 if not getattr(instance, key) == value:
                     return
-                
+            return func(sender, instance, *args, **kwargs)
+        return internal_filter
+    return inner
+
+def pre_save_old_values(**signalkwargs):
+    """Utility function to stop firing a pre_save signal if the model instance does not meet the criteria.
+
+    If the model instance is new, the signal will not fire.
+
+    The old values of the instance must meet the criteria in the kwargs dictionary"""
+    def inner(func):
+        def internal_filter(sender = None, instance = None, *args, **kwargs):
             if sender.objects.filter(pk=instance.pk).count() == 0:
-                if len(oldValuesMustBe.items()) > 0:
-                    return #new instance cant have old values
-                return func(sender, instance, *args, **kwargs)#new item, values have always changed, fire
-                
+                return
+            old = sender.objects.get(pk = instance.pk)
+            for key, value in signalkwargs.items():
+                if not hasattr(instance, key):
+                    raise Exception("Attribute " + key + " does not exist")
+                oldValue = getattr(old, key)
+                if value != oldValue:
+                    return
+            return func(sender, instance, *args, **kwargs)
+        return internal_filter
+    return inner
+
+def pre_save_change_in_values(*valuesMustHaveChanged):
+    """Utility function to stop firing a pre_save signal if the model instance does not meet the criteria.
+
+    If the model instance is new, the signal will not fire.
+
+    The values of the instance must have changed in the new data content"""
+    def inner(func):
+        def internal_filter(sender = None, instance = None, *args, **kwargs):
+            if sender.objects.filter(pk=instance.pk).count() == 0:
+                return
             old = sender.objects.get(pk = instance.pk)
             for key in valuesMustHaveChanged:
                 if not hasattr(instance, key):
                     raise Exception("Attribute " + key + " does not exist")
                 oldValue = getattr(old, key)
                 if getattr(instance, key) == oldValue:
-                    return #Value did not change, so dont fire
-                
-            for key, value in oldValuesMustBe.items():
-                if not hasattr(instance, key):
-                    raise Exception("Attribute " + key + " does not exist")
-                oldValue = getattr(old, key)
-                if value != oldValue:
-                    return #Value wasnt right in the previous state, so dont fire
-                
-            return func(sender, instance, *args, **kwargs)#All checks passed, fire
+                    return
+            return func(sender, instance, *args, **kwargs)
         return internal_filter
     return inner
+
+# def pre_save_value_filter(newValuesMustContain={}, valuesMustHaveChanged = [], oldValuesMustBe={}):
+#     """Utility function to stop firing a signal if the model instance does not meet the criteria.
+#     hasValues is a dictionary of the values that must be set in the new data content of the model.
+#     changeinAnyValues is a list of values that must be changed in the new data content of the model.
+#     Prevents firing of signal on object creation"""
+#     def inner(func):
+#         def internal_filter(sender = None, instance = None, *args, **kwargs):
+#             for (key, value) in newValuesMustContain.items():
+#                 if not hasattr(instance, key):
+#                     raise Exception("Attribute " + key + " does not exist")
+#                 if not getattr(instance, key) == value:
+#                     return
+                
+#             if sender.objects.filter(pk=instance.pk).count() == 0:
+#                 if len(oldValuesMustBe.items()) > 0:
+#                     return #new instance cant have old values
+#                 return func(sender, instance, *args, **kwargs)#new item, values have always changed, fire
+                
+#             old = sender.objects.get(pk = instance.pk)
+#             for key in valuesMustHaveChanged:
+#                 if not hasattr(instance, key):
+#                     raise Exception("Attribute " + key + " does not exist")
+#                 oldValue = getattr(old, key)
+#                 if getattr(instance, key) == oldValue:
+#                     return #Value did not change, so dont fire
+                
+#             for key, value in oldValuesMustBe.items():
+#                 if not hasattr(instance, key):
+#                     raise Exception("Attribute " + key + " does not exist")
+#                 oldValue = getattr(old, key)
+#                 if value != oldValue:
+#                     return #Value wasnt right in the previous state, so dont fire
+                
+#             return func(sender, instance, *args, **kwargs)#All checks passed, fire
+#         return internal_filter
+#     return inner
 
 
 
