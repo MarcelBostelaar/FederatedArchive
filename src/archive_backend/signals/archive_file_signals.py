f38c705748ed777b2247f78bfc94fab90dcaca55
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2025-01-06 22:54:13 +0100

    xc

--- src/archive_backend/signals/archive_file_signals.py
+++ src/archive_backend/signals/archive_file_signals.py
@@ -12,4 +12,5 @@ from .util import (post_save_new_item,
 @post_save_new_item()
 @post_save_is_local_model(False)
 def NewRemoteFile(sender = None, instance = None, *args, **kwargs):
+    #TODO call a jobified function so that individual file failure doesnt stop the whole process
     raise NotImplementedError("Remote files download")
